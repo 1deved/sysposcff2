1b8c1b00215c9689222ebadc765e2b3fb1d8b09b
e <e@e>
2026-08-03 18:24:19 -0500

    Conecta Apps Script con protección de duplicados

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 // URL del Web App de Google Apps Script (REEMPLAZAR CON LA URL REAL)
 const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbw6XmKLLK9fifEyNwaKYuGVTbvWdXBcb3kMIaTeZlFmYcSGDrAIQkDK4yX-MfO9EylY/exec";
+  "https://script.google.com/macros/s/AKfycby-Hfxrx9jNqelnihL4appk6Ew5exQqvDD-RwTf79HF47Dt-9dAuA0mT7Hj3scUjgtw/exec";
 
 // Estado de la aplicación
 let state = {
